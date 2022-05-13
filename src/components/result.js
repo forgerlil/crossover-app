a492c6732345d0e477c680fd5a1beecbfa6e3081
Christian Scholtysik <christianscholtysik@gmail.com>
2022-05-13 15:55:20 +0200

    add content to results

--- src/components/result.js
+++ src/components/result.js
@@ -3,12 +3,12 @@ import input from "./input";
 
 const Results = ({ props }) => {
   return (
-    <>
-      <div id="result-field">
+    <div id="result-field">
+      <div>
         {props.query.amount} {props.query.to}
       </div>
       <div id="ex-txt">{`Exchange rate as of ${props.date}`}</div>
-    </>
+    </div>
   );
 };
 
