5f2f997af8d3151ecff22799944f658f53a00b41
Lilian Forger <lilianforger@gmail.com>
2022-05-13 13:21:02 +0200

    Fetch successfully set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import fetchData from './components/API-fetch';
 
 function App() {
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    setData(fetchData('USD', 'EUR', '100'))
+  }, [])
+
   return (
+    data &&
     <div className="App">
+      {console.log(data)}
       The crossover one-day app will be here soon!
     </div>
   );
